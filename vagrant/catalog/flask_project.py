5aa9c5ed941651a5be11fa5ba833eba32f9986fb
Julia Kieserman <jbk67@georgetown.edu>
2017-06-17 13:45:29 -0500

    set up template inheritance

--- vagrant/catalog/flask_project.py
+++ vagrant/catalog/flask_project.py
@@ -1,16 +1,35 @@
+#from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask_bootstrap import Bootstrap
+from flask.ext.scss import Scss
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Movie
+from flask import session as login_session
+import random
+import string
 
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-# import names of database tables -- from database_setup import Base, Restaurant, MenuItem
-#DBSession = sessionmaker(bind=engine)
-#session = DBSession()
+app = Flask(__name__, static_url_path='/static')
+Scss(app, static_dir='static', asset_dir='assets')
+
+engine = create_engine('sqlite:///moviereviews.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/home')
 def homePage():
-	return render_template('home.html')
+	movies = session.query(Movie).order_by(asc(Movie.title))
+	return render_template('home.html', movies = movies)
+
+@app.route('/movie/<int:movie_id>/')
+def showMovie(movie_id):
+	movie = session.query(Movie).filter_by(id=movie_id).one()
+	print('movie obj')
+	print(movie)
+	return render_template('movie.html', movie=movie)
 
 if __name__ == '__main__':
 	app.debug = True
