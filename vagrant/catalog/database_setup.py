0f8b756bc9387fdd5484715c22e09fbedf5bd017
Julia Kieserman <jbk67@georgetown.edu>
2017-06-17 13:48:04 -0500

    add ratings to database schema

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -7,34 +5,32 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-	__tablename__ = 'user'
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
+class Person(Base):
+	__tablename__ = 'person'
 
-class UserRating(Base):
-	__tablename__ = 'rating'
 	id = Column(Integer, primary_key=True)
-	user = Column(Integer, ForeignKey('user.id'))
-	movie = Column(Integer, ForeignKey('movie.id'))
-	created = Column(DateTime, default=datetime.datetime.utcnow)
+	first_name = Column(String(250), nullable=False)
+	last_name = Column(String(250), nullable=False)
+	school = Column(String(250))
 
 class Movie(Base):
 	__tablename__ = 'movie'
 
 	id = Column(Integer, primary_key=True)
 	title = Column(String(250), nullable=False)
-	poster = Column(String(250))
+	poster = Column(String(250), nullable=False)
 	summary = Column(String(250), nullable=False)
-	industry_rating = Column(Enum('G', 'PG', 'PG13', 'R', 'NC', 'U', name='industry_rating_types'))
+	release_date = Column(String(250), nullable=False)
+	director_id = Column(Integer, ForeignKey('person.id'))
+	director = relationship(Person)
 
-class MovieRating(Base):
-	__tablename__ = 'movie_rating'
+class Rating(Base):
+	__tablename__ = 'rating'
+
 	id = Column(Integer, primary_key=True)
+	rating = Column(Integer, nullable=False)
 	movie_id = Column(Integer, ForeignKey('movie.id'))
-	user_rating = Column(Integer, ForeignKey('rating.id'))
 
 engine = create_engine('sqlite:///moviereviews.db')
-
 
 Base.metadata.create_all(engine)
