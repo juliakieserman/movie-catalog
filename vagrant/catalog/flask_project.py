32a3a14c9a14d5186a7c07e0a7e5c4784f2e38b9
Julia Kieserman <jbk67@georgetown.edu>
2017-06-18 10:55:35 -0500

    add create movie page and page to view directors

--- vagrant/catalog/flask_project.py
+++ vagrant/catalog/flask_project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, url_for
 from flask_bootstrap import Bootstrap
 from flask_scss import Scss
 from sqlalchemy import create_engine, asc
@@ -12,7 +12,6 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
 import json
-from flask import make_response
 import requests
 
 app = Flask(__name__, static_url_path='/static')
@@ -29,29 +28,29 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.context_processor
-def inject_user():
+def if_user():
 	isUser = ''
-	if 'username' not in login_session:
+	loggedIn = False
+	if 'username' in login_session is False:
 		isUser = 'Log In'
 	else:
 		isUser = 'Log Out'
-	return dict(isUser = isUser)
+		loggedIn = True
+	return dict(isUser = isUser, loggedIn = loggedIn)
 
 @app.route('/')
 @app.route('/home')
 def homePage():
-	print('does this solve it?')
 	if 'username' in login_session:
 		q = session.query(UserProfile.id).filter(UserProfile.email == login_session['email'])
-    	#if session.query(q.exists()).scalar() == False:
-    	newUser = UserProfile(email=login_session['email'], name=login_session['username'], picture_url=login_session['picture'])
-    	session.add(newUser)
-    	session.commit()
-    	print('made new user!')
-    	print(newUser)
+    	if session.query(q.exists()).scalar() == False:
+    		newUser = UserProfile(email=login_session['email'], name=login_session['username'], picture_url=login_session['picture'])
+    		session.add(newUser)
+    		session.commit()
+    		print('made new user!')
+    		print(newUser)
 
-	movies = session.query(Movie).order_by(asc(Movie.title))
-	return render_template('home.html', movies = movies)
+	return render_template('home.html')
 
 @app.route('/login')
 def showLogin():
@@ -144,7 +143,10 @@ def gconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
-    access_token = login_session['access_token']
+    credentials = login_session['credentials'].to_json()
+    print('credentials')
+    print(credentials)
+    access_token = credentials.access_token
     print 'In gdisconnect access token is %s', access_token
     print 'User name is: ' 
     print login_session['username']
@@ -178,14 +180,82 @@ def showUser():
 	if 'username' not in login_session:
 		return redirect('/login')
 	user = session.query(UserProfile).filter_by(email=login_session['email']).one()
+	print(user.email)
+	print(user.name)
+	print(user.picture_url)
 	return render_template('profile.html', user=user)
 
+@app.route('/directors')
+def viewDirectors():
+	directors = session.query(Person).order_by(asc(Person.last_name))
+	return render_template('directors.html', directors=directors)
 
 @app.route('/movie/<int:movie_id>/')
 def showMovie(movie_id):
 	movie = session.query(Movie).filter_by(id=movie_id).one()
 	ratings = session.query(Rating).filter_by(movie_id = movie_id)
 	return render_template('movie.html', movie=movie)
+
+@app.route('/movies')
+def viewMovies():
+	movies = session.query(Movie).order_by(asc(Movie.title))
+	return render_template('movies.html', movies = movies)
+
+@app.route('/movies/add', methods=['GET', 'POST'])
+def newMovie():
+	if 'username' not in login_session:
+		return redirect('/login')
+	if request.method == 'GET':
+		return render_template('newMovie.html')
+	else:
+		lName = request.form['last_name']
+		fName = request.form['first_name']
+		school = request.form['school']
+		title = request.form['title']
+		# create person object first, if it doesn't already exist
+		# NOTE: assuming no two people have the same name
+		newPerson = session.query(Person).filter(Person.first_name == fName).filter(Person.last_name == lName)
+		if newPerson is True:
+			print('in dis loop')
+			newPerson = session.query(Person).filter_by(first_name=fName, last_name=lName)
+		else:
+			print('this is the loop)')
+			newPerson = Person(first_name=fName, last_name=lName, school=school)
+			session.add(newPerson)
+			session.commit()
+
+		user = session.query(UserProfile).filter_by(email=login_session['email']).one()
+		print(user)
+		movieObj = createMovieObj(title, newPerson)
+
+		return redirect('/movies')
+
+def createMovieObj(movie, moviePerson):
+	data_string = ("https://api.themoviedb.org/3/search/movie?api_key"
+               "=0bf21ff60196bf22bce9136c1db1da7c&query=")
+
+	search_params = ''
+	for word in movie.split():
+		search_params += word+'+'
+	search_movie = data_string + search_params
+
+	r = requests.get(search_movie)
+	response = r.json()['results'][0]
+	title = response['original_title']
+	summary = response['overview']
+	poster = 'http://image.tmdb.org/t/p/w185/' + response['poster_path']
+	release_date = response['release_date']
+
+	newMovie = Movie(title=title, summary=summary, poster=poster, 
+		release_date=release_date, director=moviePerson)
+
+	session.add(newMovie)
+	session.commit()
+	return newMovie
+
+@app.route('/submit_rating', methods=['POST'])
+def submitRating():
+	print(request.form['star5'])
 
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
